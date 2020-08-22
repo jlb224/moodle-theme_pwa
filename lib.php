bf70fc1342ea330a65aa7cb18747061c4a94b024
Jo Beaver <jobeaveris@hotmail.com>
2020-08-22 13:42:44 +0100

    Override footer and navbar layout

--- lib.php
+++ lib.php
@@ -39,3 +39,34 @@ function theme_pwa_get_main_scss_content($theme) {
 
     return $scss;
 }
+
+
+// function theme_pwa_get_main_scss_content($theme) {
+//     global $CFG;
+
+//     $scss = '';
+//     $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+//     $fs = get_file_storage();
+
+//     $context = context_system::instance();
+//     if ($filename == 'default.scss') {
+//         // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+//         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+//     } else if ($filename == 'plain.scss') {
+//         // We still load the default preset files directly from the boost theme. No sense in duplicating them.
+//         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+//     } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_pwa', 'preset', 0, '/', $filename))) {
+//         // This preset file was fetched from the file area for theme_pwa and not theme_boost (see the line above).
+//         $scss .= $presetfile->get_content();
+//     } else {
+//         // Safety fallback - maybe new installs etc.
+//         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+//     }
+
+//     // pwa scss.
+//     $pwavariables = file_get_contents("$CFG->dirroot/theme/pwa/scss/defaultvariables.scss");
+//     $pwa = file_get_contents("$CFG->dirroot/theme/pwa/scss/styles.scss");
+
+//     // Combine them together.
+//     return $pwavariables . "\n" . $scss . "\n" . $pwa;
+// }
